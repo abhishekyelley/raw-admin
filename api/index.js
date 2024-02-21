2507df7d19ba363a763f0256a6dd6e9d4a53b758
Abhishek Yelley <yabhishek.19.cse@anits.edu.in>
2024-02-21 20:02:08 +0530

    Patched adminjs with patch-package

--- api/index.js
+++ api/index.js
@@ -5,6 +5,23 @@ import AdminJS from "adminjs";
 import { buildRouter } from "@adminjs/express";
 import express from "express";
 
+
+// resource: Transaction,
+//     options: {
+//     properties: {
+//         price: {
+//             type: 'currency',
+//       },
+//         phone: {
+//             type: 'phone',
+//       },
+//     },
+//     navigation: {
+//         name: 'App',
+//             icon: 'Settings',
+//     }
+// }
+
 const staticsURL = new URL("/", process.env.NEXT_PUBLIC_URL);
 const adminjs = new AdminJS(
     {
